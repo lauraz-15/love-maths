c4495e5387044762ed60de6475bc69f7b02cd53a
lauraz-15 <laura.zvirbule90@gmail.com>
2021-12-27 21:25:04 +0000

    Add structure to functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
-console.log("test");
+console.log("Connected");
+   
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
