be9d6ca11d268e0c52a957c317df4bceac53c94b
lauraz-15 <laura.zvirbule90@gmail.com>
2021-12-27 22:43:18 +0000

    Display addition qustion to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition")
 });
 
 /** 
@@ -19,12 +21,21 @@ document.addEventListener("DOMContentLoaded", function() {
  * the users choice is determined
  */
 
-function runGame() {
+function runGame(gameType) {
   let num1 = Math.floor(Math.random() * 25 +1);
   let num2 = Math.floor(Math.random() * 25 +1);
-}
 
-runGame()
+  if (gameType === "addition") {
+      displayAdditionQuestion(num1, num2) 
+    } else {
+          alert(`unknown game Type: ${gameType}`)
+          throw `unknown game Type: ${gameType}. Aborting!`;
+      }
+    }
+
+
+
+
 
 function checkAnswer() {
 
@@ -42,8 +53,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
